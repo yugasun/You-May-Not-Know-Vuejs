d48825961b5e71ac629814028b4fe1c59b0d2483
yugasun <yuga_sun@163.com>
2021-02-02 19:28:26 +0800

    chore: fix deploy script

--- build/deploy.sh
+++ build/deploy.sh
@@ -12,6 +12,6 @@ git config user.email 'yuga_sun@163.com'
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:yugasun/you-may-not-know-vuejs.git master:gh-pages
+git push -f https://github.com/yugasun/you-may-not-know-vuejs.git master:gh-pages
 
 cd -
